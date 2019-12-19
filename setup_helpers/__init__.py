7f831adcd0b8c9482b353c8f3aa6997d28518f1c
本間喜明 <yoshiaki@honmayoshiakinoMacBook-Pro.local>
2019-12-20 01:34:26 +0900

    fix file name

--- setup_helpers/__init__.py
+++ setup_helpers/__init__.py
@@ -2,4 +2,4 @@ from .setup_dataset import setup_dataset  # NOQA
 from .setup_model import setup_model, freeze_params  # NOQA
 from .setup_extension import setup_extension  # NOQA
 from .setup_transform import setup_transform  # NOQA
-from .setup_solver import setup_optimizer, add_hook_optimizer  # NOQA
+from .setup_optimizer import setup_optimizer, add_hook_optimizer  # NOQA
